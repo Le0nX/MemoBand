3f0558ad754ff809d2b593b661908257801b9323
Le0nX <cloudmindone@gmail.com>
2019-12-02 13:23:26 +0300

    init

---
+++ MemoBand/MemoBand/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoBand
+//
+//  Created by Denis Nefedov on 02.12.2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
